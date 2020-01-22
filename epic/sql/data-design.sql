42f2041e5d176f4531380ab39a58877c5c72fa9f
Vladimir Arias-Antonov <ariasantonov93@gmail.com>
2020-01-22 14:38:16 -0800

    fixed the error in DDL from "order" to Purchase.

--- epic/sql/data-design.sql
+++ epic/sql/data-design.sql
@@ -1,78 +1,34 @@
-# DROP TABLE IF EXISTS chat;
-# DROP TABLE IF EXISTS profile2;
-# DROP TABLE IF EXISTS profile1;
-#
-#
-# CREATE TABLE profile1 (
-#
-# 	steamEmail1 VARCHAR(128) NOT NULL,
-#    steamId1 BINARY(16) NOT NULL,
-# 	steamName1 CHAR(32) NOT NULL,
-# 	steamProfileDetails1 VARCHAR (8000),
-# 	steamProfilePic1 VARCHAR(140),
-# 	steamTheme1 VARCHAR(140),
-#
-# 	UNIQUE(steamEmail1),
-# 	PRIMARY KEY(steamID1)
-# );
-#
-# CREATE TABLE profile2 (
-#
-# 	steamEmail2 VARCHAR(128) NOT NULL,
-#    steamId2 BINARY(16) NOT NULL,
-# 	steamName2 CHAR(32) NOT NULL,
-# 	steamProfileDetails2 VARCHAR (8000),
-# 	steamProfilePic2 VARCHAR(140),
-# 	steamTheme2 VARCHAR(140),
-#
-# 	UNIQUE(steamEmail2),
-# 	PRIMARY KEY(steamId2)
-# );
-#
-# CREATE TABLE chat (
-# 	chatDate DATETIME(6) NOT NULL,
-# 	chatEmote VARCHAR(255),
-#    chatId BINARY(16) NOT NULL,
-# 	chatProfileId BINARY(16) NOT NULL,
-# 	chatSteamID BINARY(16) NOT NULL,
-#
-# 	INDEX(chatProfileID),
-# 	FOREIGN  KEY(chatSteamID) REFERENCES profile1(steamId1),#might need profile2 and steamid2 original was REFERENCES profile(steamId)
-# 	PRIMARY KEY(chatID)
+DROP TABLE IF EXISTS Product;
+DROP TABLE IF EXISTS Purchase;
+DROP TABLE IF EXISTS SteamAccount;
 
-# );
-
-DROP TABLE IF EXISTS 'Product';
-DROP TABLE IF EXISTS 'Order';
-DROP TABLE IF EXISTS 'SteamAccount';
-
-CREATE TABLE 'SteamAccount' (
-	steamId BINARY(32) NOT NULL,
-	email varchar(32) NOT NULL,
-	phone varchar(10) NOT NULL,
+CREATE TABLE SteamAccount (
+	steamId BINARY(16) NOT NULL,
+	email VARCHAR(32) NOT NULL,
+	phone VARCHAR(10) NOT NULL,
 	steamName VARCHAR(32) NOT NULL,
+
 	UNIQUE(email),
 	PRIMARY KEY (steamId)
 );
 
-CREATE TABLE 'Order' (
-	orderId BINARY(32) NOT NULL,
-	customerName VARCHAR(32) NOT NULL,
-	productId BINARY(32) NOT NULL,
-	purchaseDate DATETIME(6) NOT NULL,
-	steamId BINARY(32) NOT NULL,
-
-
-	PRIMARY KEY (orderId),
-	FOREIGN KEY (steamId) REFERENCES SteamAccount(steamId),
-	FOREIGN KEY (productId) REFERENCES Product(productId)
-);
-
-CREATE TABLE 'Product' (
-	productId BINARY(16),
+CREATE TABLE Product (
+   productSteamId BINARY(16) NOT NULL,
+	productId BINARY(16) NOT NULL,
 	quantity VARCHAR(255) NOT NULL,
 	productName VARCHAR(50) NOT NULL,
 
 	PRIMARY KEY (productId)
 );
 
+CREATE TABLE Purchase (
+	purchaseId BINARY(16) NOT NULL,
+	purchaseProductId BINARY(16) NOT NULL,
+	purchaseDate DATETIME(6) NOT NULL,
+	purchaseSteamId BINARY(16) NOT NULL,
+
+	INDEX (purchaseSteamId),
+	PRIMARY KEY (purchaseId),
+	FOREIGN KEY (purchaseSteamId) REFERENCES SteamAccount(steamId),
+	FOREIGN KEY (purchaseProductId) REFERENCES Product(productId)
+);
