40dee0669efeda64900b49595b7e30f059ef9da2
Vladimir Arias-Antonov <ariasantonov93@gmail.com>
2020-01-22 15:02:29 -0800

    sarted oml

--- epic/sql/data-design.sql
+++ epic/sql/data-design.sql
@@ -1,30 +1,31 @@
-DROP TABLE IF EXISTS Product;
 DROP TABLE IF EXISTS Purchase;
+DROP TABLE IF EXISTS Product;
 DROP TABLE IF EXISTS SteamAccount;
 
 CREATE TABLE SteamAccount (
 	steamId BINARY(16) NOT NULL,
-	email VARCHAR(32) NOT NULL,
-	phone VARCHAR(10) NOT NULL,
+	steamEmail VARCHAR(32) NOT NULL,
 	steamName VARCHAR(32) NOT NULL,
+	steamPhone VARCHAR(10) NOT NULL,
 
-	UNIQUE(email),
+
+	UNIQUE(steamEmail),
 	PRIMARY KEY (steamId)
 );
 
 CREATE TABLE Product (
    productSteamId BINARY(16) NOT NULL,
 	productId BINARY(16) NOT NULL,
-	quantity VARCHAR(255) NOT NULL,
 	productName VARCHAR(50) NOT NULL,
+	productQuantity VARCHAR(255) NOT NULL,
 
 	PRIMARY KEY (productId)
 );
 
 CREATE TABLE Purchase (
 	purchaseId BINARY(16) NOT NULL,
-	purchaseProductId BINARY(16) NOT NULL,
 	purchaseDate DATETIME(6) NOT NULL,
+	purchaseProductId BINARY(16) NOT NULL,
 	purchaseSteamId BINARY(16) NOT NULL,
 
 	INDEX (purchaseSteamId),
